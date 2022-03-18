bbe9367892bca86342a2cd5605a57b56c48efd3b
Thiago Cedran Santos <thiagocdn@gmail.com>
2022-03-17 21:53:50 -0400

    Added and corrected webpack

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,3 @@
 import React from 'react';
+import { App } from './app'
 
-function App() {
-  return <h1>Hello World!</h1>
-}
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,20 @@
 const path = require('path');
 
-module.exporta = {
+module.exports = {
   entry: path.resolve(__dirname, 'src', 'index.jsx'),
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js'
+    filename: 'bundle.js',
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'],
   },
   module: {
     rules: [
       {
         test: /\.jsx$/,
         exclude: /node_modules/,
-        user: 'babel-loader'
+        use: 'babel-loader',
       }
     ],
   }
