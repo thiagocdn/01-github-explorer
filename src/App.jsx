19a0ee3e17c51440cd9039e6d4ac5038d2c4f2b7
Thiago Cedran Santos <thiagocdn@gmail.com>
2022-03-31 20:09:31 -0300

    criado counter com conceito de estado

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,11 @@
+import { Counter } from "./components/Counter";
 import { RepositoryList } from "./components/RepositoryList";
 
 export function App() {
-  return <RepositoryList />
+  return (
+  <>
+    <RepositoryList />
+    <Counter />
+  </>
+  )
 }
